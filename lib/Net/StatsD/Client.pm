51649bdf95244c5fa9187144bcfd75f58f833748
Steve Ivy <sivy@sixapart.com>
2011-02-18 06:32:12 -0700

    usage and license for perldoc

--- lib/Net/StatsD/Client.pm
+++ lib/Net/StatsD/Client.pm
@@ -15,6 +15,20 @@ The brilliant devops folks at Etsy released (and posted about) an awesome little
 
 Statsd sits in front of the Graphite metrics server, providing a simple API for applications to send stats over UDP. UDP is "old tech" but is fire-and-forget -- clients don't have to wait for a response to keep processing.
 
+=head1 USAGE
+
+    use Net::StatsD::Client;
+
+    my $client = Net::StatsD::Client->new();
+
+    $client->timing('perl_test.timing',500);
+    $client->increment('perl_test.inc_int');
+    $client->decrement('perl_test.inc_int');
+
+=head1 License
+
+This code is released under the same license as Perl itself.
+
 =cut
 
 use IO::Socket::INET;
